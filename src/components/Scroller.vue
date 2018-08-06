73a18b16e4f86f35a2e65373f6148ccf4a364c94
zhangyiqi0910 <1562070422@qq.com>
2018-08-06 17:21:32 +0800

    首页及电影栏

---
+++ src/components/Scroller.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="scroller">
+    <!--滚动栏标题-->
+    <div class="header">
+      <h2>{{title}}</h2>
+      <a href="#">更多</a>
+    </div>
+    <!--滚动栏内容-->
+    <div class="content">
+      <slot name="promItem"></slot>
+      <ul class="hasCover" v-if="type === 'hasCover'">
+        <li v-for="item in items">
+          <router-link :to="'subject/' + item.id" append>
+            <img v-if="item.images" :src="item.images.large" alt="">
+            <span class="title">{{item.title}}</span>
+            <rating v-if="item.rating" :rating="item.rating"></rating>
+          </router-link>
+        </li>
+      </ul>
+      <ul class="onlyString" v-if="type === 'onlyString'">
+        <li v-for="item in items">
+          <a :href="item.href" v-for="!item.line" :style="{color: item.color}">{{item.title}}</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  import Rating from './Rating'
+
+  export default {
+    name: 'Scroller',
+    props: ['title', 'type', 'items'],
+    components: { Rating },
+    data () {
+      return {}
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .scroller {
+    padding-top: 1rem;
+  }
+
+  .header {
+    height: 2.6rem;
+    line-height: 2.6rem;
+    padding: 0 1.6rem;
+
+    a {
+      float: right;
+      font-size: 1.44rem;
+      &:last-child {
+        color: #42bd56;
+      }
+    }
+
+    h2 {
+      display: inline-block;
+    }
+  }
+
+  .content {
+    box-sizing: content-box;
+
+    ul {
+      padding: 0.8rem 0;
+    }
+  }
+
+  .hasCover {
+    overflow: hidden;
+    white-space: nowrap;
+    text-align: center;
+
+    .title {
+      display: block;
+      max-width: 100%;
+      margin-top: 1rem;
+      line-height: 1.6rem;
+      font-size: 1.6rem;
+      color: #111;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      word-wrap: normal;
+    }
+
+    li {
+      display: inline-block;
+      width: 10rem;
+      margin-left: 1rem;
+    }
+
+    li:first-child {
+      padding-left: 0.8rem;
+    }
+
+    img {
+      height: 15rem;
+    }
+  }
+
+  .onlyString {
+    overflow-x: auto;
+    white-space: normal;
+
+    li {
+      display: inline-block;
+      margin: 0 0 0.8rem 1.6rem;
+      font-size: 1.6rem;
+      border: 0.1rem solid;
+      border-radius: 0.4rem;
+      vertical-align: middle;
+    }
+
+    a {
+      height: 5rem;
+      line-height: 5rem;
+      padding: 0 2.4rem;
+      letter-spacing: 0.16rem;
+      overflow: auto;
+      display: block;
+      text-align: center;
+    }
+
+    li:empty {
+      width: 100%;
+      display: block;
+      height: 0.1rem;
+      border: 0;
+      margin: 0;
+    }
+  }
+</style>
---
+++ src/views/MovieView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="movie-view has-header">
+    <!--vue-scroll：实现上拉加载下拉刷新-->
+    <scroller title="影院热映" type="hasCover" :items="hotMovies"></scroller>
+    <scroller title="免费在线观影" type="hasCover" :items="topMovies"></scroller>
+    <scroller title="新片速递" type="hasCover" :items="newMovies"></scroller>
+    <scroller title="发现好电影" type="onlyString" :items="movieTags"></scroller>
+    <!--分类浏览-->
+    <types></types>
+    <!--下载APP-->
+    <download-app></download-app>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+
+<style scoped>
+
+</style>
